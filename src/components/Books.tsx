7e9819ef817796479cf3de5b203dd94ce83eec99
Egor <kerminegor@gmail.com>
2022-07-08 13:33:27 +0300

    5.3

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -5,22 +5,12 @@ export default function Books(props:{books:BookState[]}){
     const {books} = props
     return  <div className="books">
         {books.map((book:BookState,i:number)=>{
-            if(book.image === undefined) {
-                return <Link className="book" key={i} to={`/${book.id}`}>  
-                {book.name} 
+            return <Link className="book" key={i} to={`/${book.id}`}>
+                {book.image !== undefined && <img src={book.image} alt="" />}
+                <p>{book.name}</p>
                 {Array.isArray(book.authors) && <p>authors:{book.authors.join(", ")}</p> }
                 {Array.isArray(book.categories) && <p>categories:{book.categories[0]}</p> }
-                </Link>
-            }
-            else {
-                return <Link className="book" key={i} to={`/${book.id}`}>
-                    <img src={book.image} alt="" />
-                    <p>{book.name}</p>
-                    {Array.isArray(book.authors) && <p>authors:{book.authors.join(", ")}</p> }
-                    {Array.isArray(book.categories) && <p>categories:{book.categories[0]}</p> }
-                </Link>
-                
-            }
+            </Link>
     })}
     </div>
 }
